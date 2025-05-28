a23cb25e7e41000f8a32d315ae891aaca2d120f4
jhlee <jun73045928@gmail.com>
2025-05-28 15:18:52 +0900

    Initial commit

---
+++ src/test/kotlin/org/swenbe/kotlinpractice/KotlinPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.swenbe.kotlinpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
